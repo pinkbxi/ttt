6980f58313844a2a470e657f0b3af73018bd37df
Domantas <81468296+pinkbxi@users.noreply.github.com>
2025-09-14 23:38:27 +0300

    The main files
    Added the main program files.

---
+++ TicTacToe/logic.js
@@ -0,0 +1,100 @@
+var num = 0;
+var fs = document.getElementById("l1").textContent;
+var ss = document.getElementById("l2").textContent;
+var ts = document.getElementById("l3").textContent;
+var fs = document.getElementById("l4").textContent;
+var fis = document.getElementById("l5").textContent;
+var sis = document.getElementById("l6").textContent;
+var ses = document.getElementById("l7").textContent;
+var es = document.getElementById("l8").textContent;
+var ns = document.getElementById("l9").textContent;
+function l1(){
+    if(num % 2 == 0){
+        document.getElementById("l1").textContent = "X";
+        num++;
+    }
+    else{
+        document.getElementById("l1").textContent = "O";
+        num++;
+    }
+}
+function l2(){
+    if(num % 2 == 0){
+        document.getElementById("l2").textContent = "X";
+        num++;
+    }
+    else{
+        document.getElementById("l2").textContent = "O";
+        num++;
+    }
+}
+function l3(){
+    if(num % 2 == 0){
+        document.getElementById("l3").textContent = "X";
+        num++;
+    }
+    else{
+        document.getElementById("l3").textContent = "O";
+        num++;
+    }
+}
+function l4(){
+    if(num % 2 == 0){
+        document.getElementById("l4").textContent = "X";
+        num++;
+    }
+    else{
+        document.getElementById("l4").textContent = "O";
+        num++;
+    }
+}
+function l5(){
+    if(num % 2 == 0){
+        document.getElementById("l5").textContent = "X";
+        num++;
+    }
+    else{
+        document.getElementById("l5").textContent = "O";
+        num++;
+    }
+}
+function l6(){
+    if(num % 2 == 0){
+        document.getElementById("l6").textContent = "X";
+        num++;
+    }
+    else{
+        document.getElementById("l6").textContent = "O";
+        num++;
+    }
+}
+function l7(){
+    if(num % 2 == 0){
+        document.getElementById("l7").textContent = "X";
+        num++;
+    }
+    else{
+        document.getElementById("l7").textContent = "O";
+        num++;
+    }
+}
+function l8(){
+    if(num % 2 == 0){
+        document.getElementById("l8").textContent = "X";
+        num++;
+    }
+    else{
+        document.getElementById("l8").textContent = "O";
+        num++;
+    }
+}
+function l9(){
+    if(num % 2 == 0){
+        document.getElementById("l9").textContent = "X";
+        num++;
+    }
+    else{
+        document.getElementById("l9").textContent = "O";
+        num++;
+    }
+}
